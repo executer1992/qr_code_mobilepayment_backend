bba6cf574322aa42e8e2337f2038ad9f61c665a6
executer1992 <filioski@gmail.com>
2020-01-06 20:22:19 +0100

    added drop tables

--- init.sql
+++ init.sql
@@ -6,3 +6,22 @@ CREATE TABLE IF NOT EXISTS users (
   created_date TIMESTAMP,
   modified_date TIMESTAMP
 )
+
+CREATE TABLE IF NOT EXISTS funds (
+  user_id UUID NOT NULL,
+  balance NUMERIC(8, 4),
+  PRIMARY KEY (user_id),
+  CONSTRAINT f_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+)
+
+CREATE TABLE IF NOT EXISTS funds_history (
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  transcation_ammount NUMERIC(8, 4),
+  transaction_partner VARCHAR(128) NOT NULL,
+  transaction_recieved BOOLEAN NOT NULL,
+  transaction_date TIMESTAMP,
+  PRIMARY KEY (user_id),
+  CONSTRAINT fh_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+)
+
