9b38e444a484b0c551c92d0415870c4b01a83d6a
executer1992 <filioski@gmail.com>
2020-01-06 20:30:11 +0100

    corections

--- init.sql
+++ init.sql
@@ -8,20 +8,19 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS funds (
+  fund_id UUID PRIMARY KEY,
   user_id UUID NOT NULL,
   balance NUMERIC(8, 4),
-  PRIMARY KEY (user_id),
-  CONSTRAINT f_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS funds_history (
-  id UUID PRIMARY KEY,
+  fund_history_id UUID PRIMARY KEY,
   user_id UUID NOT NULL,
   transcation_ammount NUMERIC(8, 4),
   transaction_partner VARCHAR(128) NOT NULL,
   transaction_recieved BOOLEAN NOT NULL,
   transaction_date TIMESTAMP,
-  PRIMARY KEY (user_id),
-  CONSTRAINT fh_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
